6615c93638b25843deba135a9538b761faaf5c1e
nihal <nihalshantha470@gmail.com>
2022-09-06 14:52:31 +0530

    update application.properties Added Employee.java, EmployeeController.java, EmployeeRepository.java, ResourceNotFoundException.java

---
+++ src/main/java/com/app/restapi/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.app.restapi.repository;
+
+import com.app.restapi.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
